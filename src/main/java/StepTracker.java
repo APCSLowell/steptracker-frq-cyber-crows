c7926eed870b9fd3d2d1fbad1280a199a1e6dff3
cybercrow <malcolmbarrykao@gmail.com>
2025-04-23 16:17:48 -0700

    Update StepTracker.java

--- src/main/java/StepTracker.java
+++ src/main/java/StepTracker.java
@@ -1,5 +1,22 @@
 import java.util.ArrayList;
 public class StepTracker
 {
- /* to be implemented here */
+private int total, days, count; 
+public StepTracker(int goal){
+ G=goal; 
+ total=0; 
+ days=0; 
+ count=0; 
+}
+ public void addDailySteps(int steps){
+ total+=steps;  
+ if(steps>=G)
+   count++;
+ days++; 
+ }
+ public int activeDays(){
+  return days; 
+ }
+ public double AverageSteps()
+  return (double) total/days; 
 } 
